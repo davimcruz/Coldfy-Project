3122cba29c0ad24425e5f74337cb3324116d7d93
oDustdev <fab.aquiles123@gmail.com>
2023-10-19 22:05:42 -0300

    .

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ class MyDocument extends Document {
             integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
             crossorigin="anonymous"
           ></script>
-          <script src="https://coldfy.com.br/javascript/animations.js"></script>
+          <script src="https://coldfy.com.br/javascript/Animations.js"></script>
         </Head>
         <body>
           <Main />
